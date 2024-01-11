43324b37876466575a2ed472476a913670c02ae2
Nikhil <nsivapur@uwo.ca>
2024-01-11 14:35:29 -0500

    first commit
    Signed-off-by: Nikhil <nsivapur@uwo.ca>

---
+++ src/main/java/com/takehome/locateAlpha/LocateAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.takehome.locateAlpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocateAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocateAlphaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takehome/locateAlpha/LocateAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takehome.locateAlpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocateAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
